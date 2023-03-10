566fc6a412cca91348a78d6b120a25f8fbc682c0
Полина Грунина <linaborisovsky@yandex.ru>
2023-03-10 21:50:18 +0300

    red_1.2

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 
 from . import views
 
-app_name='posts'
-namespace='posts'
+app_name = 'posts'
+namespace = 'posts'
 
 urlpatterns = [
     path('', views.index, name='index'),
